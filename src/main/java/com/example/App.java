9a2265055a526304ad72944186fce936ec9815cc
RiccardoGrandi <grandi.riccardo@itismeucci.com>
2023-11-07 13:34:12 +0100

    server reso multi-thread

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,47 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Socket s = new Socket("localhost", 3000);
+            System.out.println("Connessione effettuata");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            Integer numeroBiglietti = 0;
+
+            Scanner tastiera = new Scanner(System.in);
+            
+            String userString = "";
+            do {
+                System.out.println("> Digita D per visualizzare la disponibilità, A per acquistare un biglietto o Q per uscire: ");
+                userString = tastiera.nextLine(); 
+                out.writeBytes(userString +"\n");
+
+                String stringaRicevuta = in.readLine();   
+                numeroBiglietti = Integer.parseInt(stringaRicevuta);   
+
+                if (numeroBiglietti != 0 && userString.equals("D")) {
+                    System.out.println("> Disponibili " + numeroBiglietti + " biglietti");
+                }
+                
+                else if (numeroBiglietti != 0 && userString.equals("A")) {
+                        System.out.println("> Biglietto acquistato");
+                }
+            } while (!userString.equals("Q") || numeroBiglietti != 0);            
+            
+            if (numeroBiglietti == 0) {
+                System.out.println("> Biglietti esauriti");
+            }
+            else{
+                System.out.println("> Esci");
+            }
+
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Something went wrong");
+            System.exit(1);
+        }
     }
 }
